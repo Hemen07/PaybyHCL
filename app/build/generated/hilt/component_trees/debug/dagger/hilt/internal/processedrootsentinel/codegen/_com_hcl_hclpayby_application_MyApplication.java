a434ff76bd74a1d7628b2550ed3907cf870e78cc
Hemen <talukdar.hemen@hotmail.com>
2024-05-13 22:23:13 +0530

    removed un-necessary build folder.

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_hcl_hclpayby_application_MyApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.hcl.hclpayby.application.MyApplication"
-)
-public final class _com_hcl_hclpayby_application_MyApplication {
-}
